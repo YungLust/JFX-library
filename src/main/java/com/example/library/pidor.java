6f2170834f0708eb90743aa4629b229bb9eb172b
YungLust <artyr.yanchuk2@gmail.com>
2025-03-18 20:36:04 +0200

    init

---
+++ src/main/java/com/example/library/pidor.java
@@ -0,0 +1,11 @@
+package com.example.library;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class pidor extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
